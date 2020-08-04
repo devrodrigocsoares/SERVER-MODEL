1c418b9aade2762d0786c40ef776321b28031e75
Rodrigo Soares <rodrigo.soares0642@outlook.com.br>
2020-08-04 13:50:07 -0300

    MODEL SERVER for backend

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import express, { request, response } from 'express';
+
+//importar os controllers aqui dentro.
+
+const routes = express.Router();
+
+//const itensController = new ItemsController(); - Instanciar o controller por ser uma classe.
+
+
+//routes.get('/rota', 'instacia.nomedometodod(index|create|show|delete))';
+//ex = routes.post('/points', pointsController.create);
+
+
+export default routes;
